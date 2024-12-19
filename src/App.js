7684b7d5f996c5b26968377655cd7479dcd4644c
bharathipriya-123 <109799663+bharathipriya-123@users.noreply.github.com>
2024-12-19 22:08:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 
 import './App.css';
-import Header from './component/Header';
+
 import Home from './component/Home';
-import ExpenseList from './component/ExpenseList';
-import ExpenseForm from './component/ExpenseForm';
+
 
 
 
 function App() {
   return (
     <div className='conatiner-1'>
-      {/* <Header/> */}
+     
       <Home/>  
     </div>
   );
